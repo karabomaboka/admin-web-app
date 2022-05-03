0f5bcc032c9ef3b4462de92950aff764e391e513
Karabo Maboka <51972141+karabomaboka@users.noreply.github.com>
2022-05-03 22:02:38 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,50 +1,21 @@
 <?php
-// Start session management and include necessary functions
-session_start();
-require_once('model/database.php');
-require_once('model/admin_db.php');
-
-// Get the action to perform
-$action = filter_input(INPUT_POST, 'action');
-if ($action == NULL) {
-    $action = filter_input(INPUT_GET, 'action');
-    if ($action == NULL) {
-        $action = 'show_admin_menu';
-    }
-}
-
-// If the user isn't logged in, force the user to login
-if (!isset($_SESSION['is_valid_admin'])) {
-    $action = 'login';
-}
-
-// Perform the specified action
-switch($action) {
-    case 'login':
-        $email = filter_input(INPUT_POST, 'email');
-        $password = filter_input(INPUT_POST, 'password');
-        if (is_valid_admin_login($email, $password)) {
-            $_SESSION['is_valid_admin'] = true;
-            include('view/admin_menu.php');
-        } else {
-            $login_message = 'You must login to view this page.';
-            include('view/login.php');
-        }
-        break;
-    case 'show_admin_menu':
-        include('view/admin_menu.php');
-        break;
-    case 'show_product_manager':
-        include('view/product_manager.php');
-        break;
-    case 'show_order_manager':
-        include('view/order_manager.php');
-        break;
-    case 'logout':
-        $_SESSION = array();   // Clear all session data from memory
-        session_destroy();     // Clean up the session ID
-        $login_message = 'You have been logged out.';
-        include('view/login.php');
-        break;
-}
+    require_once('util/secure_conn.php');  // require a secure connection
+    require_once('util/valid_admin.php');  // require a valid admin user
 ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>My Guitar Shop</title>
+        <link rel="stylesheet" type="text/css" href="main.css"/>
+    </head>
+    <body>
+        <header>
+            <h1>My Guitar Shop</h1>
+        </header>
+        <main>
+            <h1>Admin Menu</h1>
+            <p><a href="product_manager.php">Product Manager</a></p>
+            <p><a href="order_manager.php">Order Manager</a></p>
+        </main>
+    </body>
+</html>
